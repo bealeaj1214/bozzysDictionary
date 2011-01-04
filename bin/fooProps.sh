0f4bf20c7d4b4338e037293c31d2e6975af21642
Alan Beale <bealea@execpc.com>
2011-01-04 10:53:29 -0600

    corrected error in property generation script

--- bin/fooProps.sh
+++ bin/fooProps.sh
@@ -3,6 +3,6 @@
 export LETTERS="bravo charlie delta echo foxtrot gold hotel india juliet kilo lima mike"
 for letter in $LETTERS; 
 do 
-    echo "<prop key=\"$letter\"> NATO phonetic letter $letter<prop>" 
+    echo "<prop key=\"$letter\"> NATO phonetic letter $letter</prop>" 
 done
 
